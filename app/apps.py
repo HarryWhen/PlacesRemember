21973812ae001502f2d10237f0b1af8bec106a41
unknown <Hagai>
2023-06-07 15:44:00 +0500

    Fix function-redifined

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,17 @@
-from django.apps import AppConfig
+"""
+Module containing Django applications.
+
+This module contains the definitions of Django applications used in the project.
+"""
+from django.apps import AppConfig as Config
 
 
-class AppConfig(AppConfig):
+class AppConfig(Config):
+    """
+    Application configuration.
+
+    This class defines the configuration for the application,
+    including its name and various parameters.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
